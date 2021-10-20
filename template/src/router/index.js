b8c6a7bdf83c48cba938da602ea5ebdc338558a2
sen <hdy9515@dingtalk.com>
2021-10-20 17:13:25 +0800

    FIX: hooks/useRouterLoader

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useLoader } from '@/utils/hooks/useRouterLoader'
 
 const routes = [
-  { path: '/', name: 'Home', component: useLoader(import('../views/home')), },
-  { path: '/:pathMatch(.*)*', name: 'NotFound', component: useLoader(import('@/views/notFound')), },
+  { path: '/', name: 'Home', component: useLoader(() => import('../views/home')), },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: useLoader(() => import('@/views/notFound')), },
 ]
 
 const router = createRouter({
--- template/src/utils/hooks/useRouterLoader.js
+++ template/src/utils/hooks/useRouterLoader.js
@@ -1,9 +1,10 @@
 import NProgress from 'nprogress'
 
 export const useLoader = (componentLoader, _options = {}) => {
+  console.assert(typeof componentLoader === 'function', 'componentLoader not a function!')
   return () => {
     NProgress.start()
-    return componentLoader.finally(() => {
+    return componentLoader().finally(() => {
       NProgress.done()
     })
   }
