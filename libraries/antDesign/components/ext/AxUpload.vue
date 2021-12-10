f77fa95d66e0bbac6e3111d37f9703f1043bb62a
sen <hdy9515@dingtalk.com>
2021-12-10 15:53:09 +0800

    FEAT: add AxUpload for antDesign ext

---
+++ libraries/antDesign/components/ext/AxUpload.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-upload @change="selectFileChange" :beforeUpload="() => false" :show-upload-list="false">
+    <slot></slot>
+  </a-upload>
+</template>
+
+<script lang='jsx'>
+import { reactive, toRefs, defineComponent } from 'vue'
+
+export default defineComponent({
+  setup (_props, _ctx) {
+    const state = reactive({
+      selectFileChange({file, fileList}) {
+        console.log(...arguments)
+      },
+    })
+    return { ...toRefs(state) }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
